9e3c57da73ddd5708b05b6161fa668308a8f5ee3
@mpttsub <yuukisteam1523@gmail.com>
2017-07-18 19:51:32 +0900

    Update moverasp.py

--- moverasp.py
+++ moverasp.py
@@ -1,6 +1,7 @@
+#coding:utf-8
 import RPi.GPIO as GPIO
 import time
-BUZZER = 27
+BUZZER = 18
 TONE_HIGH = 970
 TONE_LOW = 770
 RB = 21
@@ -16,33 +17,43 @@ GPIO.setup(LB, GPIO.OUT, initial=GPIO.LOW)
 
 
 while True:
-    com = input("前:1  後:2  終了:3")
-    if com=="1":
-        GPIO.output(RF,1)
-        GPIO.output(LF,1)
-        time.sleep(1.0)
-        GPIO.output(RF,0)
-        GPIO.output(LF,0)
+        com = input("front:1  back:2  end:3  buzzer:4")
+        if com==1:
+                GPIO.output(RF,1)
+                GPIO.output(LF,1)
+                time.sleep(1.0)
+                #GPIO.output(RF,0)
+                #GPIO.output(LF,0)
 
-    elif com=="2":
-        GPIO.output(RB,1)
-        GPIO.output(LB,1)
-        time.sleep(1.0)
-        GPIO.output(RB,0)
-        GPIO.output(LB,0)
-    
-    elif com=="3":
-        break
+        elif com==2:
+                GPIO.output(RB,1)
+                GPIO.output(LB,1)
+                time.sleep(1.0)
+                #GPIO.output(RB,0)
+                #GPIO.output(LB,0)
 
-    elif com=="4":
-        p = GPIO.PWM(BUZZER, TONE_HIGH)
-        p.start(50)
+        elif com==3:
+                break
 
-    for i in range(5):
-        p.ChangeFrequency(TONE_HIGH)
-        time.sleep(0.65)
-        p.ChangeFrequency(TONE_LOW)
-        time.sleep(0.65)
-    p.stop()
+        elif com==4:
+                p = GPIO.PWM(BUZZER, 1)
+                p.start(50)
+                p.ChangeFrequency(970)
+                time.sleep(0.65)
+                p.ChangeFrequency(770)
+                time.sleep(0.65)
+                p.ChangeFrequency(970)
+                time.sleep(0.65)
+                p.ChangeFrequency(770)
+                time.sleep(0.65)
+                p.ChangeFrequency(970)
+                time.sleep(0.65)
+                p.ChangeFrequency(770)
+                time.sleep(0.65)
+                p.ChangeFrequency(970)
+                time.sleep(0.65)
+                p.ChangeFrequency(770)
+                time.sleep(0.65)
+                p.stop()
 
 GPIO.cleanup()
